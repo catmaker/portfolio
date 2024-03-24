2fd514e93717b4b2981d9639712f789418cce306
catmaker <rzt0317@nate.com>
2024-03-25 05:29:13 +0900

    feat : Completed final version of Intro component

--- src/app/components/Intro/Intro.tsx
+++ src/app/components/Intro/Intro.tsx
@@ -2,23 +2,66 @@ import React from "react";
 import Image from "next/image";
 const Intro = () => {
   return (
-    <div className="flex text-black min-h-20 items-center justify-center flex-col">
-      <div className="flex w-4/5 justify-center mt-48">
-        <Image
-          className="rounded-full"
-          src="/img/profile.jpg"
-          alt=""
-          width={300}
-          height={300}
-        ></Image>
+    <>
+      <div className="flex flex-col md:flex-row text-black w-4/5 m-auto min-h-20 items-center justify-center pt-32">
+        <div className="flex w-4/5 justify-center">
+          <Image
+            className="rounded-full"
+            src="/img/profile.jpg"
+            width={200}
+            height={200}
+            sizes="100vw"
+            alt=""
+          />
+        </div>
+        <div className="flex w-3/5 mt-6 flex-col">
+          <h3 className=" text-lg sm:text-lg md:text-xl lg:text-2xl xl:text-3xl font-bold transition-size duration-200 ">
+            신지수 (Shin Ji Su)
+          </h3>
+          <p className="font-bold mt-4 text-xl text-custom-orange">Contact</p>
+          <div className="flex gap-2 items-center">
+            <span className="w-80px pr-8 text-sm">phone.</span>
+            <span className="text-custom-gray text-xs border-b-1 border-custom-gray">
+              010-4951-4437
+            </span>
+          </div>
+          <div className="flex gap-2 items-center">
+            <span className="w-80px pr-8 text-sm">email.</span>
+            <span className="text-custom-gray text-xs border-b-1 border-custom-gray">
+              rzt0357@gmail.com
+            </span>
+          </div>
+          <div className="flex gap-2 items-center">
+            <span className="w-80px pr-9 text-sm">github.</span>
+            <span className="text-custom-gray text-xs border-b-1 border-custom-gray">
+              <a href="https://github.com/catmaker" target="_blank">
+                https://github.com/catmaker
+              </a>
+            </span>
+          </div>
+          <p className="font-bold mt-4 text-xl text-custom-orange">Skill</p>
+          <div className="flex gap-2 items-center">
+            <span className="w-80px pr-8 text-sm">Deploy.</span>
+            <span className="text-custom-gray text-xs border-b-1 border-custom-gray">
+              Firebase, Vercel
+            </span>
+          </div>
+          <div className="flex gap-2 items-center">
+            <span className="w-80px pr-8 text-sm">Language.</span>
+            <span className="text-custom-gray text-xs border-b-1 border-custom-gray">
+              Typescript, Javascript, Styled-Component
+            </span>
+          </div>
+          <div className="flex gap-2 items-center">
+            <span className="w-80px pr-8 text-sm">FW/LB.</span>
+            <span className="text-custom-gray text-xs border-b-1 border-custom-gray">
+              Next.js, React, Node.js
+            </span>
+          </div>
+        </div>
       </div>
-      <div className="flex w-4/5 flex-col justify-center items-center mt-10">
-        <h3 className="text-lg sm:text-lg md:text-xl lg:text-2xl xl:text-3xl font-press-start font-bold transition-size duration-200">
-          FRONTEND DEVELOPER
-        </h3>
-        <p className="text-lg font-bold mt-1">신지수</p>
-      </div>
-    </div>
+      <div className="border-b-2 w-4/5 m-auto mt-16 border-grey"></div>
+    </>
   );
 };
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,29 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
       colors: {
         "custom-orange": "#FC836C",
+        "custom-gray": "#979797",
       },
       width: {
         "90": "90%",
+        "80px": "80px",
+        "120px": "120px",
+      },
+      borderColor: {
+        "custom-gray": "#979797",
+      },
+      borderWidth: {
+        "0.5": "0.5px",
+        "1": "1px",
+        "2": "2px",
       },
       animation: {
         shake: "shake 0.82s cubic-bezier(.36,.07,.19,.97) both",
