79856783ff8e4e610058c2f3ca1c45065c0fbd6b
catmaker <rzt0317@nate.com>
2024-03-25 05:32:47 +0900

    fix : Removed h-screen from main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,15 @@ import Header from "./components/header/page";
 import Intro from "@/app/components/Intro/page";
 import About from "@/app/components/about/page";
 import Project from "@/app/components/project/page";
+import Contact from "@/app/components/contact/page";
 export default function Home() {
   return (
-    <main className="min-h-screen bg-white text-black w-full bg-[url('/img/bubble.png')] bg-cover bg-center bg-fixed">
+    <main className=" bg-white text-black w-full bg-[url('/img/bubble.png')] bg-cover bg-center bg-fixed">
       <Header />
-      <div className="h-screen">
-        <Intro />
-      </div>
-      <div className="h-screen">
-        <About />
-      </div>
-      <div className="h-screen">
-        <Project />
-      </div>
+      <Intro />
+      <About />
+      <Project />
+      <Contact />
     </main>
   );
 }
