4b8ddc4322df6989caf5ce4a010d52c1023dc731
catmaker <rzt0317@nate.com>
2024-03-25 06:10:24 +0900

    update : Updated about section

--- src/app/components/about/About.tsx
+++ src/app/components/about/About.tsx
@@ -2,55 +2,24 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen">
-      <div className="flex gap-4 md:gap-8 lg:gap-12 xl:gap-16 mb-4 md:mb-8 lg:mb-12 xl:mb-16 w-4/5 max-w-5xl">
-        <div className="flex border p-4 w-1/2 flex-col bg-white text-black rounded-xl min-h-48 min-w-48 gap-4 transition-shadow duration-300 hover:shadow-lg hover:animate-shake shadow-md">
-          <div className="flex justify-center items-center font-bold">
-            ABOUT ME
-          </div>
-          <div className="flex flex-col gap-1 text-xs md:text-base">
-            <p>신지수</p>
-            <p>1997.05.23</p>
-            <p>대림대학교 방송음향영상과</p>
-            <p className="text-xs">음향, 영상 복수전공</p>
-          </div>
-        </div>
-        <div className="flex border p-4 w-1/2 flex-col bg-white text-black rounded-xl min-h-48 min-w-48 gap-4 transition-shadow duration-300 hover:shadow-lg hover:animate-shake shadow-md">
-          <div className="flex justify-center items-center font-bold">
-            CHANNEL
-          </div>
-          <div className="flex flex-col gap-1 text-xs md:text-base">
-            <a href="https://github.com/catmaker" target="_blank">
-              https://github.com/catmaker
-            </a>
-            <a href="notion.io" target="_blank">
-              notion.io
-            </a>
-          </div>
+    <>
+      <div className="flex-col md:flex-row text-black w-4/5 m-auto min-h-20 items-center justify-center pt-12">
+        <h1 className="text-4xl font-bold text-custom-orange">Introduction</h1>
+        <div>
+          <p className="text-sm sm:text-base leading-relaxed mt-4">
+            안녕하세요, 프론트엔드 지망생 신지수입니다.
+            <br /> 최근 8개월간 프론트엔드 개발에 몰두하여 3개의 프로젝트를
+            진행하며 열정을 키웠습니다.
+            <br /> 개발스터디를 통해 팀원들과 적극적인 활동을 통해 성장하고
+            있습니다.
+            <br /> 사용자 중심의 인터페이스와 탁월한 경험을 만들기 위해 노력하고
+            있으며,
+            <br /> 현업에서 활약하기 위해 지속적인 학습과 개발을 하고 있습니다.
+          </p>
         </div>
       </div>
-      <div className="flex gap-4 md:gap-8 lg:gap-12 xl:gap-16 mb-4 md:mb-8 lg:mb-12 xl:mb-16 w-4/5 max-w-5xl">
-        <div className="flex border p-4 w-1/2 flex-col bg-white text-black rounded-xl min-h-48 min-w-48 gap-4 transition-shadow duration-300 hover:shadow-lg hover:animate-shake shadow-md">
-          <div className="flex justify-center items-center font-bold">
-            EDUCATION
-          </div>
-          <div className="flex flex-col gap-1 text-xs md:text-base">
-            <p className="text-xs">2023-08-16 - 2023-12-27</p>
-            <p>멀티잇 프론트엔드 개발(React)</p>
-          </div>
-        </div>
-        <div className="flex border p-4 w-1/2 flex-col bg-white text-black rounded-xl min-h-48 min-w-48 gap-4 transition-shadow duration-300 hover:shadow-lg hover:animate-shake shadow-md">
-          <div className="flex justify-center items-center font-bold">WORK</div>
-          <div className="flex flex-col gap-1 text-xs md:text-base">
-            <p className="text-xs">2023.06~2023.08</p>
-            <p className="md:hidden">엔젤로 음향 쇼핑몰</p>
-            <p className="hidden md:block">
-              엔젤로 음향 쇼핑몰 프론트 유지보수
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
+      <div className="border-b-2 w-4/5 m-auto mt-16 border-grey"></div>
+    </>
   );
 };
 
