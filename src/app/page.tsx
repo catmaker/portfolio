4f2512c4e3f74ccc1414cc3e3d528f36fc5fbb99
catmaker <rzt0317@nate.com>
2024-03-24 09:00:35 +0900

    feat : Add Background Img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import About from "@/app/components/about/page";
 import Project from "@/app/components/project/page";
 export default function Home() {
   return (
-    <main className="min-h-screen bg-white text-black w-full">
+    <main className="min-h-screen bg-white text-black w-full bg-[url('/img/bubble.png')] bg-cover bg-center bg-fixed">
       <Header />
       <div className="h-screen">
         <Intro />
